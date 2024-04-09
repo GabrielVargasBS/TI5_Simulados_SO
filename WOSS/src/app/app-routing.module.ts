7ccad824cf6c502751b78a6a588f5ba9d60f92d2
Camilla Teodoro <camillateodoro@hotmail.com>
2024-04-09 14:57:11 -0300

    Adiciona componentes e configura rota

--- WOSS/src/app/app-routing.module.ts
+++ WOSS/src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { IndexComponent } from './home/pages/index/index.component';
+import { SimulacaoComponent } from './home/pages/simulacao/simulacao.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', redirectTo: '/index',pathMatch: 'full'
+  },
+  {
+    path: 'index', component: IndexComponent
+  },
+  {
+    path: 'simulacao', component: SimulacaoComponent
+  },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- WOSS/src/app/app.module.ts
+++ WOSS/src/app/app.module.ts
@@ -3,10 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './home/components/header/header.component';
+import { IndexComponent } from './home/pages/index/index.component';
+import { SimulacaoComponent } from './home/pages/simulacao/simulacao.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    IndexComponent,
+    SimulacaoComponent
   ],
   imports: [
     BrowserModule,
